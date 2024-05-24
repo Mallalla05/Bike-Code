4bd683cd42aada8ddfe384f8b65c70010930bf37
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-24 10:14:02 -0600

    main.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,5 +1,25 @@
+#include "tienda.h"
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout << "Hello World!\n";
+  Tienda tienda;
+  tienda.creEjemplo();
+  cout << "\nInventario Completo: " << endl;
+  tienda.muestraBicicletas();
+  cout << "\nBicicleta de montaña: " << endl;
+  tienda.muestraBicicletas("Montana");
+  cout << "\nBicicleta de ruta: " << endl;
+  tienda.muestraBicicletas("ruta");
+  cout << "\nBicicleta urbana: " << endl;
+  tienda.muestraBicicletas("urbana");
+  cout << "\nBicicleta BMX: " << endl;
+  tienda.muestraBicicletas("bmx");
+  cout << "\nBicicleta Ebike: " << endl;
+  tienda.muestraBicicletas("Ebike");
+  cout << "\nBicicleta Gravel: " << endl;
+  tienda.muestraBicicletas("Gravel");
+  double valorTotal = tienda.calculaTotalInventario();
+  cout << "\nValor del inventario: $" << valorTotal << endl;
+  return 0;
 }
