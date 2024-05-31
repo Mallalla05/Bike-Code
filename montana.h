5f72c70e04d4905257564b29c657ffbe71fb2d89
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 20:59:48 -0600

    montana.h

--- montana.h
+++ montana.h
@@ -1,26 +1,24 @@
 #ifndef MONTANA_H_
 #define MONTANA_H_
+
 #include "bicicleta.h"
 
-class Montana: public Bicicleta {
+class Montana : public Bicicleta {
 private:
-  string suspension;
+    string suspension;
 
 public:
+    Montana(string nom, string mar, int a, double p, string col, double pre, string susp)
+        : Bicicleta(nom, mar, a, p, col, pre), suspension(susp) {}
 
-Montana (string nom,string mar,int a,double p,string col,double pre,string susp):
-  Bicicleta (nom,mar,a,p,col,pre),suspension (susp){}
-
-  string getTipo()const override{
-    return "Montaña";
-  }
-  string toString()const override{
-    stringstream ss;
-    ss<<"ipo: Montaña, Nombre: "<<nombre<< ", Marca: "<<marca<< ", Año: "<<ano<< ", Peso: " <<peso<< "kg, Color: " << color << ", Precio $: " <<precio<<  ", Suspension: " <<suspension<<  "\n"
-    return ss.str();
-
-
-
-      }
+    string getTipo() const override { return "Montaña"; }
+    string toString() const override {
+        stringstream ss;
+        ss << "Tipo: Montaña, Nombre: " << nombre << ", Marca: " << marca
+           << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
+           << ", Precio $: " << precio << ", Suspensión: " << suspension << "\n";
+        return ss.str();
+    }
 };
+
 #endif
