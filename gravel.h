a67c30e9b2f17774403933d49fe4ccf06cd0c715
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 20:58:13 -0600

    gravel.h

--- gravel.h
+++ gravel.h
@@ -1,25 +1,24 @@
 #ifndef GRAVEL_H_
 #define GRAVEL_H_
+
 #include "bicicleta.h"
 
-class Gravel: public Bicicleta {
+class Gravel : public Bicicleta {
 private:
-  string transmision;
+    string transmision;
 
 public:
+    Gravel(string nom, string mar, int a, double p, string col, double pre, string trans)
+        : Bicicleta(nom, mar, a, p, col, pre), transmision(trans) {}
 
-  Gravel (string nom,string mar,int a,double p,string col,double pre,string susp):
-    Bicicleta (nom,mar,a,p,col,pre),transmision (trans){}
-
-    string getTipo()const override{
-      return "Gravel";
+    string getTipo() const override { return "Gravel"; }
+    string toString() const override {
+        stringstream ss;
+        ss << "Tipo: Gravel, Nombre: " << nombre << ", Marca: " << marca
+           << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
+           << ", Precio $: " << precio << ", Transmisión: " << transmision << "\n";
+        return ss.str();
     }
-    string toString()const override{
-      stringstream ss;
-      ss<<"ipo: Gravel, Nombre: "<<nombre<< ", Marca: "<<marca<< ", Año: "<<ano<< ", Peso: " <<peso<< "kg, Color: " << color << ", Precio $: " <<precio<<  ", Transmision: " <<transmision<<  "\n"
-      return ss.str();
-
-
-      }
 };
+
 #endif
