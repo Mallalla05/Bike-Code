3e12e2a94dd31678a769f1f6479728eac9483b89
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 20:56:45 -0600

    bmx.h

--- bmx.h
+++ bmx.h
@@ -1,26 +1,24 @@
 #ifndef BMX_H_
 #define BMX_H_
+
 #include "bicicleta.h"
 
-class Bmx: public Bicicleta {
+class Bmx : public Bicicleta {
 private:
-  string tipoFreno;
+    string tipoFreno;
 
 public:
+    Bmx(string nom, string mar, int a, double p, string col, double pre, string freno)
+        : Bicicleta(nom, mar, a, p, col, pre), tipoFreno(freno) {}
 
-Bmx(string nom,string mar,int a,double p,string col,double pre,string susp):
-  Bicicleta (nom,mar,a,p,col,pre),tipoFreno (freno){}
-
-  string getTipo()const override{
-    return "Bmx";
-  }
-  string toString()const override{
-    stringstream ss;
-    ss<<"ipo: Bmx, Nombre: "<<nombre<< ", Marca: "<<marca<< ", Año: "<<ano<< ", Peso: " <<peso<< "kg, Color: " << color << ", Precio $: " <<precio<<  ", Tipo de freno: " <<tipoFreno<<  "\n"
-    return ss.str();
-
-
-
-      }
+    string getTipo() const override { return "Bmx"; }
+    string toString() const override {
+        stringstream ss;
+        ss << "Tipo: Bmx, Nombre: " << nombre << ", Marca: " << marca
+           << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
+           << ", Precio $: " << precio << ", Tipo de freno: " << tipoFreno << "\n";
+        return ss.str();
+    }
 };
+
 #endif
