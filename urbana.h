db1b964f5501276cd1b0ef8b490b01c80a1af4b6
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 21:02:59 -0600

    urbana.h

--- urbana.h
+++ urbana.h
@@ -1,25 +1,25 @@
 #ifndef URBANA_H_
 #define URBANA_H_
+
 #include "bicicleta.h"
 
-class Urbana: public Bicicleta {
+class Urbana : public Bicicleta {
 private:
-  int rodada;
+  float rodada;
 
 public:
+  Urbana(string nom, string mar, int a, double p, string col, double pre,
+         float rod)
+      : Bicicleta(nom, mar, a, p, col, pre), rodada(rod) {}
 
-Urbana(string nom,string mar,int a,double p,string col,double pre,string susp):
-  Bicicleta (nom,mar,a,p,col,pre),rodada(rod){}
-
-  string getTipo()const override{
-    return "Urbana";
-  }
-  string toString()const override{
+  string getTipo() const override { return "Urbana"; }
+  string toString() const override {
     stringstream ss;
-    ss<<"ipo: Urbana, Nombre: "<<nombre<< ", Marca: "<<marca<< ", Año: "<<ano<< ", Peso: " <<peso<< "kg, Color: " << color << ", Precio $: " <<precio<<  ", Rodada: " <<rodada<<  "\n"
+    ss << "Tipo: Urbana, Nombre: " << nombre << ", Marca: " << marca
+       << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
+       << ", Precio $: " << precio << ", Rodada: " << rodada << "\n";
     return ss.str();
-
-
-      }
+  }
 };
+
 #endif
