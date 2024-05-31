7b687a70449e5b0565c103d1bdf736afd530be99
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 21:00:39 -0600

    ruta.h

--- ruta.h
+++ ruta.h
@@ -1,24 +1,26 @@
 #ifndef RUTA_H_
 #define RUTA_H_
+
 #include "bicicleta.h"
 
-class Ruta: public Bicicleta {
+class Ruta : public Bicicleta {
 private:
-  string materialMarco;
+    string materialMarco;
 
 public:
-  Ruta (string nom,string mar,int a,double p,string col,double pre,string material):
-  Bicicleta (nom,mar,a,p,col,pre),materialMarco (material){}
-string getTipo()const override{
-  return "Ruta";
-}
-string toString()const override{
-  stringstream ss;
-  ss<<"ipo: Ruta, Nombre: "<<nombre<< ", Marca: "<<marca<< ", Año: "<<ano<< ", Peso: " <<peso<< "kg, Color: " << color << ", Precio $: " <<precio<<  ", Material del marco: " <<materialMarco<<  "\n"
-  return ss.str();
-  
-      }
+    Ruta(string nom, string mar, int a, double p, string col, double pre, string material)
+        : Bicicleta(nom, mar, a, p, col, pre), materialMarco(material) {}
+
+    string getTipo() const override { return "Ruta"; }
+    string toString() const override {
+        stringstream ss;
+        ss << "Tipo: Ruta, Nombre: " << nombre << ", Marca: " << marca
+           << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
+           << ", Precio $: " << precio << ", Material del marco: " << materialMarco << "\n";
+        return ss.str();
+    }
 };
+
 #endif
 
   
