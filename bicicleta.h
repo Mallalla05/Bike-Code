3b02a3f5cc4fd43f5bf75d40887863285ca343a2
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 20:56:08 -0600

    Bicicleta.h

--- bicicleta.h
+++ bicicleta.h
@@ -1,28 +1,27 @@
 #ifndef BICICLETA_H_
 #define BICICLETA_H_
+
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 class Bicicleta {
 protected:
-  sting nombre;
-  string marca;
-  int ano;
-  double peso;
-  string color;
-  double precio;
-  s
+    string nombre;
+    string marca;
+    int ano;
+    double peso;
+    string color;
+    double precio;
 
 public:
-  
-  Bicicleta (string nom,string mar,int a,double p,string col,double pre,):
-      nombre(nom),marca(mar),ano(a),peso(p),color(col),preci o(pre){}
-  virtual Bicicleta(){}
-  virtual string getTipo() const=0;
-  virtual string toTipo() const=0;
-  double getPrecio () const {
-    return precio;
-  }
+    Bicicleta(string nom, string mar, int a, double p, string col, double pre)
+        : nombre(nom), marca(mar), ano(a), peso(p), color(col), precio(pre) {}
+    virtual ~Bicicleta() {} 
+    virtual string getTipo() const = 0;
+    virtual string toString() const = 0;
+    double getPrecio() const { return precio; }
 };
 
 #endif
