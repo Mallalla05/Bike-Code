1f2b571f976e61a72688043be93c01aa4a05aa8e
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-05-30 20:59:00 -0600

    Main.cpp

--- main-2.cpp
+++ main-2.cpp
@@ -1,25 +1,35 @@
 #include "tienda.h"
 #include <iostream>
+
 using namespace std;
 
 int main() {
-  Tienda tienda;
-  tienda.creEjemplo();
-  cout << "\nInventario Completo: " << endl;
-  tienda.muestraBicicletas();
-  cout << "\nBicicleta de montaña: " << endl;
-  tienda.muestraBicicletas("Montana");
-  cout << "\nBicicleta de ruta: " << endl;
-  tienda.muestraBicicletas("ruta");
-  cout << "\nBicicleta urbana: " << endl;
-  tienda.muestraBicicletas("urbana");
-  cout << "\nBicicleta BMX: " << endl;
-  tienda.muestraBicicletas("bmx");
-  cout << "\nBicicleta Ebike: " << endl;
-  tienda.muestraBicicletas("Ebike");
-  cout << "\nBicicleta Gravel: " << endl;
-  tienda.muestraBicicletas("Gravel");
-  double valorTotal = tienda.calculaTotalInventario();
-  cout << "\nValor del inventario: $" << valorTotal << endl;
-  return 0;
+    Tienda tienda;
+    tienda.creaEjemplos();
+
+    cout << "\nInventario Completo: " << endl;
+    tienda.muestraBicicletas();
+
+    cout << "\nBicicleta de montaña: " << endl;
+    tienda.muestraBicicletas("Montaña");
+
+    cout << "\nBicicleta de ruta: " << endl;
+    tienda.muestraBicicletas("Ruta");
+
+    cout << "\nBicicleta urbana: " << endl;
+    tienda.muestraBicicletas("Urbana");
+
+    cout << "\nBicicleta BMX: " << endl;
+    tienda.muestraBicicletas("Bmx");
+
+    cout << "\nBicicleta Ebike: " << endl;
+    tienda.muestraBicicletas("Ebike");
+
+    cout << "\nBicicleta Gravel: " << endl;
+    tienda.muestraBicicletas("Gravel");
+
+    int valorTotal = tienda.calculaTotalInventario();
+    cout << "\nValor del inventario: $" << valorTotal << endl;
+
+    return 0;
 }
