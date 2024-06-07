240ad4712cded5064b5dbdcd1f5310066accd662
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-06-06 22:15:30 -0600

    ruta.h

--- ruta.h
+++ ruta.h
@@ -8,7 +8,8 @@ private:
     string materialMarco;
 
 public:
-    Ruta(string nom, string mar, int a, double p, string col, double pre, string material)
+    Ruta(string nom, string mar, int a, double p, string col, double pre, 
+        string material)
         : Bicicleta(nom, mar, a, p, col, pre), materialMarco(material) {}
 
     string getTipo() const override { return "Ruta"; }
@@ -16,12 +17,10 @@ public:
         stringstream ss;
         ss << "Tipo: Ruta, Nombre: " << nombre << ", Marca: " << marca
            << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
-           << ", Precio $: " << precio << ", Material del marco: " << materialMarco << "\n";
+           << ", Precio $: " << precio << ", Material del marco: " 
+            << materialMarco << "\n";
         return ss.str();
     }
 };
 
 #endif
-
-  
- 
