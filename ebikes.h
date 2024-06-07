621cfa802dc4e2bc3a28d2119c86fde82c89979e
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-06-06 22:02:29 -0600

    ebikes.h

--- ebikes.h
+++ ebikes.h
@@ -9,17 +9,20 @@ private:
     int potencia;
 
 public:
-    Ebike(string nom, string mar, int a, double p, string col, double pre, int aut, int pot)
+    Ebike(string nom, string mar, int a, double p, string col, double pre,
+            int aut, int pot)
         : Bicicleta(nom, mar, a, p, col, pre), autonomia(aut), potencia(pot) {}
 
     string getTipo() const override { return "Ebike"; }
     string toString() const override {
+        
         stringstream ss;
         ss << "Tipo: Ebike, Nombre: " << nombre << ", Marca: " << marca
            << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
            << ", Precio $: " << precio << ", Autonomía: " << autonomia
            << "km, Potencia: " << potencia << "W\n";
         return ss.str();
+        
     }
 };
 
