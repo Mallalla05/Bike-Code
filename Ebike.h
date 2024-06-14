5ed5bd4760e86ccc4e2f8ea6653bbc6a45ad54fb
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-06-14 01:20:28 -0600

    Ebike.h

--- Ebike.h
+++ Ebike.h
@@ -1,29 +1,44 @@
+/*
+* Proyecto: Bike code
+*Autor: Mariana Hernández Díaz
+*A01711207
+*14/06/2024
+*En esta clase se define el objeto Ebike el cual hereda de la clase abstracta que es bicicleta.
+*/
+
 #ifndef EBIKE_H_
 #define EBIKE_H_
 
-#include "bicicleta.h"
+#include "Bicicleta.h"
 
+//Se declara la clase ebike que se hereda de Bicicleta
 class Ebike : public Bicicleta {
 private:
-    int autonomia;
-    int potencia;
+//Se declara la variable especifica de esta clase
+    int autonomia;//Autonomia de la bicicleta
+    int potencia;//Potencia de la bicicleta
 
+//Son los metodos publicos de nuestro objeto
 public:
+//Se crea el constructor de nuestra clase
     Ebike(string nom, string mar, int a, double p, string col, double pre,
             int aut, int pot)
         : Bicicleta(nom, mar, a, p, col, pre), autonomia(aut), potencia(pot) {}
 
+/*En esta parte el override nos ayuda a sobreescribir el metodo de virtual 
+toString de bicicleta*/
+
     string getTipo() const override { return "Ebike"; }
     string toString() const override {
-        
+
         stringstream ss;
         ss << "Tipo: Ebike, Nombre: " << nombre << ", Marca: " << marca
            << ", Año: " << ano << ", Peso: " << peso << "kg, Color: " << color
            << ", Precio $: " << precio << ", Autonomía: " << autonomia
            << "km, Potencia: " << potencia << "W\n";
         return ss.str();
-        
+
     }
 };
 
-#endif
+#endif //EBIKE_H_
