1c42714426203fcbb8bda2f2c39a963192ddd90b
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-06-14 01:29:37 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,33 @@
-#include "tienda.h"
+/*
+ * Proyecto: Bike code
+ *Autor: Mariana Hernández Díaz
+ *A01711207
+ *14/06/2024
+ *En este main se crea el menu de nuestro programa,se definen las principales
+ *funciones que son donde podemos crear nuestros objetos y agregar a el archivo
+ *tienda.
+ */
+#include "Tienda.h" //incluye nuestro archivo de tienda
 #include <iostream>
 
 using namespace std;
-
+// En esta parte parte se genera el menu de opciones
 void muestraMenu() {
-  cout << "\n ~ Opciones a realizar ~ " << endl;
-  cout << "\n 1.- Tipos de bicicletas " << endl;
-  cout << "\n 2.- Registra una bicicleta" << endl;
-  cout << "\n 3.- Valor de inventario" << endl;
-  cout << "\n 4.- Salir" << endl;
-  cout << "\n Elige la opción: ";
+  cout << "\n ~ Opciones a realizar ~ " << endl; // Titulo del menú
+  cout << "\n 1.- Tipos de bicicletas "
+       << endl; // Opcion donde encontramos nuestro inventario de bicicletas ya
+                // agregadas.
+  cout << "\n 2.- Registra una bicicleta"
+       << endl; // Te permite agregar una bicicleta nueva al inventario
+  cout << "\n 3.- Valor de inventario"
+       << endl; // Te da el valor total de los precios que estan en inventario.
+  cout << "\n 4.- Salir" << endl; // Nos deja salir del programa
+  cout << "\n Elige la opción: "; // Da la opcion a que el usuario decida la
+                                  // opcion que quiera.
 }
 
+// En este manu es el que se deplega despues de seleccionar opcion 1 en el menú
+// anterior y escoger la modalidad de bicicleta que necesite.
 void muestraCategorias() {
   cout << "\n ~ Bicicletas dependiendo modalidad ~ " << endl;
   cout << "\n 1.- Montaña " << endl;
@@ -20,25 +36,29 @@ void muestraCategorias() {
   cout << "\n 4.- BMX" << endl;
   cout << "\n 5.- Ebike " << endl;
   cout << "\n 6.- Gravel" << endl;
-  cout << "\n Dame la modalidad: ";
+  cout << "\n Dame la modalidad: "; // Nos deja que el usuario seleccione la
+                                    // modalidad que quiere.
 }
 void mostrarBicisCategoria(Tienda &tienda, int opcion) {
   if (opcion == 1) {
-    tienda.muestraBicicletas("Montaña");
+    tienda.muestraBicicletas("Montaña"); // Muestra las bicis de montaña
   } else if (opcion == 2) {
-    tienda.muestraBicicletas("Ruta");
+    tienda.muestraBicicletas("Ruta"); // Muestra las bicis de ruta
   } else if (opcion == 3) {
-    tienda.muestraBicicletas("Urbana");
+    tienda.muestraBicicletas("Urbana"); // Muestra las bicis urbanas
   } else if (opcion == 4) {
-    tienda.muestraBicicletas("Bmx");
+    tienda.muestraBicicletas("Bmx"); // Muestra las bicis de bmx
   } else if (opcion == 5) {
-    tienda.muestraBicicletas("Ebike");
+    tienda.muestraBicicletas("Ebike"); // Muestra las ebikes
   } else if (opcion == 6) {
-    tienda.muestraBicicletas("Gravel");
+    tienda.muestraBicicletas("Gravel"); // Muestra las bicis de gravel
   } else {
     cout << "Categoría no válida." << endl;
   }
 }
+
+// Esta funcion nos permite que el usuario registre nuevas bicicletas al
+// inventario
 
 void agregarBicicleta(Tienda &tienda) {
   string nombre, marca, color, tipoFreno, suspension, transmision,
@@ -47,11 +67,14 @@ void agregarBicicleta(Tienda &tienda) {
   double peso, precio;
   float rodada;
 
-  cout << "Seleccione el tipo de bicicleta a registrar: " << endl;
+  cout << "Seleccione el tipo de bicicleta a registrar: "
+       << endl; // Muestra las opciones de bicicletas para registrar
   cout << "1. Montaña\n2. Ruta\n3. Urbana\n4. BMX\n5. Ebike\n6. Gravel\n";
-  cin >> tipo;
-  cin.ignore();
+  cin >> tipo;  // Lee la opcion que el usuario seleccione
+  cin.ignore(); // Nos da el salto de linea
 
+  // Esta parte nos permite que el usuario vaya colocando los datos de las
+  // bicis.
   cout << "Ingrese el nombre: ";
   getline(cin, nombre);
   cout << "Ingrese la marca: ";
@@ -64,9 +87,11 @@ void agregarBicicleta(Tienda &tienda) {
   cin.ignore();
   getline(cin, color);
   cout << "Ingrese el precio: ";
-  cin >> precio;
+  cin >> precio; // Lee el precio de la bicicleta
   cin.ignore();
 
+  // Esta es la parte que dependiendo de la bicicleta que el usuario decida es
+  // el detalle especifico por bici.
   if (tipo == 1) {
     cout << "Ingrese la suspensión: ";
     getline(cin, suspension);
@@ -100,22 +125,24 @@ void agregarBicicleta(Tienda &tienda) {
     tienda.agregarBicicleta(
         new Gravel(nombre, marca, ano, peso, color, precio, transmision));
   } else {
-    cout << "Tipo no válido." << endl;
+    cout << "Tipo no válido."
+         << endl; // Nos muestra si es que el tipo dado es erroneo.
   }
 }
 
 int main() {
+  // Se crea el objeto de tienda y se inicializa en ejemplo de bicicleta.
   Tienda tienda;
   tienda.creaEjemplos();
 
   int opcion;
   do {
     muestraMenu();
-    while (!(cin >> opcion)) {
+    while (!(cin >> opcion)) { // valida la opcion del usuario
       cout << "Opción no válida. Intente nuevamente: ";
       cin.clear();
     }
-
+    // Depende de la opcion que seleccione el usuario es lo que se va a realizar
     if (opcion == 1) {
       muestraCategorias();
       int categoria;
@@ -123,19 +150,23 @@ int main() {
         cout << "Categoría no válida. Intente nuevamente: ";
         cin.clear();
       }
-      mostrarBicisCategoria(tienda, categoria);
+      mostrarBicisCategoria(
+          tienda, categoria); // muestra la categoria de bici seleccionada
     } else if (opcion == 2) {
-      agregarBicicleta(tienda);
+      agregarBicicleta(tienda); // agrega una nueva bici al inventario
     } else if (opcion == 3) {
       cout << "\nValor del inventario: $" << tienda.calculaTotalInventario()
            << endl;
     } else if (opcion == 4) {
-      cout << "Saliendo..." << endl;
+      cout << "Saliendo..." << endl; // muestra el mensaje de salida
     } else {
-      cout << "Opción no válida." << endl;
+      cout << "Opción no válida."
+           << endl; // nos marca error si la opcion no es valida.
     }
 
-  } while (opcion != 4);
+  } while (opcion != 4); // deja que el codigo se siga repitiendo hasta que el
+                         // usuario nos deje salir
 
-  return 0;
+  return 0; // Se finaliza el examen
 }
+
