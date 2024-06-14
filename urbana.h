51d4fa54ed47e3cc5257b0a5d0c2e1997a14f97c
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-06-14 01:13:55 -0600

    Urbana.h

--- urbana.h
+++ urbana.h
@@ -1,17 +1,29 @@
+/*
+ * Proyecto: Bike code
+ *Autor: Mariana Hernández Díaz
+ *A01711207
+ *14/06/2024
+ *En esta clase se define el objeto urbana el cual hereda de la clase abstracta
+ *que es bicicleta.
+ */
 #ifndef URBANA_H_
 #define URBANA_H_
 
-#include "bicicleta.h"
-
+#include "Bicicleta.h"
+// Se declara la clase urbana que se hereda de Bicicleta
 class Urbana : public Bicicleta {
 private:
-  float rodada;
-
+  // Se declara la variable especifica de esta clase
+  float rodada; // rodada de la bicicleta
+  // Son los metodos publicos de nuestro objeto
 public:
+  // Se crea el constructor de nuestra clase
   Urbana(string nom, string mar, int a, double p, string col, double pre,
          float rod)
       : Bicicleta(nom, mar, a, p, col, pre), rodada(rod) {}
 
+  /* En esta parte el override nos ayuda a sobreescribir el metodo de virtual
+  toString de bicicleta*/
   string getTipo() const override { return "Urbana"; }
   string toString() const override {
     stringstream ss;
@@ -22,4 +34,5 @@ public:
   }
 };
 
-#endif
+#endif // URBANA_H_
+
